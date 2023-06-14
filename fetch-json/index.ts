89209b71cf5e2046df294cfa4578bd94b34a6944
Ayxa Chaverra <achaverrar@gmail.com>
2023-06-14 10:52:58 -0500

    use data type annotations to spot errors in a function

--- fetch-json/index.ts
+++ fetch-json/index.ts
@@ -11,7 +11,12 @@ interface Todo {
 axios.get(url).then((response) => {
   const todo = response.data as Todo;
   const { id, title, completed } = todo;
+
+  logTodo(id, title, completed);
+});
+
+const logTodo = (id: number, title: string, completed: boolean) => {
   console.log(`The Todo of ID ${id}
   Has a title of ${title}.
   Is it finished? ${completed}`);
-});
+};
