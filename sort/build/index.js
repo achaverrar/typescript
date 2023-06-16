cd11748a8edc14930f1b3bda760939e97ddf1c9a
Ayxa Chaverra <achaverrar@gmail.com>
2023-06-16 09:44:03 -0500

    implement a class to sort arrays using the bubble sort sorting algorithm

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,2 +1,22 @@
 "use strict";
-console.log("hi there");
+class Sorter {
+    constructor(collection) {
+        this.collection = collection;
+    }
+    sort() {
+        const { length } = this.collection;
+        for (let i = 0; i < length; i++) {
+            const ref = this.collection[i];
+            for (let j = 0; j < length - 1 - i; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    const leftHandCopy = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHandCopy;
+                }
+            }
+        }
+    }
+}
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter);
