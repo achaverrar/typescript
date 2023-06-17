e36646f496e2da4635a93a201579154ce7defe3a
Ayxa Chaverra <achaverrar@gmail.com>
2023-06-17 15:00:25 -0500

    turn the Sorter class into an abstract class from which the NumbersCollection class can inherit and restructure the code accordingly

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,15 +1,11 @@
-import { CharactersCollection } from "./CharactersCollection";
+//import { CharactersCollection } from "./CharactersCollection";
 import { NumbersCollection } from "./NumbersCollection";
-import { Sorter } from "./Sorter";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const charactersCollection = new CharactersCollection("uOiEa");
+//const charactersCollection = new CharactersCollection("uOiEa");
 
-const numbersSorter = new Sorter(numbersCollection);
-const charactersSorter = new Sorter(charactersCollection);
-
-numbersSorter.sort();
-charactersSorter.sort();
+numbersCollection.sort();
+//charactersSorter.sort();
 
 console.log(numbersCollection.data);
-console.log(charactersCollection.data);
+//console.log(charactersCollection.data);
