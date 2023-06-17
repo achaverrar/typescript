9ee3427abd8f80e246a38649442766ed7864bdf8
Ayxa Chaverra <achaverrar@gmail.com>
2023-06-17 11:34:40 -0500

    use the Sortable interface to refactor the code

--- sort/src/Sorter.ts
+++ sort/src/Sorter.ts
@@ -1,14 +1,12 @@
 export class Sorter {
-  constructor(public collection: number[]) {}
+  constructor(public collection: Sortable) {}
 
   sort(): void {
     const { length } = this.collection;
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - 1 - i; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHandCopy = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHandCopy;
+        if (this.collection.compare(j, j + 1)) {
+          this.collection.swap(j, j + 1);
         }
       }
     }
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,5 +1,8 @@
+import { NumbersCollection } from "./NumbersCollection";
 import { Sorter } from "./Sorter";
 
-const sorter = new Sorter([10, 3, -5, 0]);
-sorter.sort();
-console.log(sorter);
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+const numbersSorter = new Sorter(numbersCollection);
+numbersSorter.sort();
+
+console.log(numbersCollection.data);
