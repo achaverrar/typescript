3b428531923179e34241db261fd3d284a1c5455f
Ayxa Chaverra <achaverrar@gmail.com>
2023-06-17 11:37:26 -0500

    test the sort method on an instance of the CharactersCollection class

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,8 +1,15 @@
+import { CharactersCollection } from "./CharactersCollection";
 import { NumbersCollection } from "./NumbersCollection";
 import { Sorter } from "./Sorter";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+const charactersCollection = new CharactersCollection("uOiEa");
+
 const numbersSorter = new Sorter(numbersCollection);
+const charactersSorter = new Sorter(charactersCollection);
+
 numbersSorter.sort();
+charactersSorter.sort();
 
 console.log(numbersCollection.data);
+console.log(charactersCollection.data);
