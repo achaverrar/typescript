e8b4b223a1d98f27ff6f77a57eb2b49ee976db47
Ayxa Chaverra <achaverrar@gmail.com>
2023-06-17 15:04:00 -0500

    make the CharactersCollection class extend from the Sorter one and refactor the code accordinginly

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,11 +1,11 @@
-//import { CharactersCollection } from "./CharactersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 import { NumbersCollection } from "./NumbersCollection";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-//const charactersCollection = new CharactersCollection("uOiEa");
+const charactersCollection = new CharactersCollection("uOiEa");
 
 numbersCollection.sort();
-//charactersSorter.sort();
+charactersCollection.sort();
 
 console.log(numbersCollection.data);
-//console.log(charactersCollection.data);
+console.log(charactersCollection.data);
