382e8d2de7a8afc488b706ace8f5cfa8b720cd5f
Ayxa Chaverra <achaverrar@gmail.com>
2023-06-17 20:52:51 -0500

    illustrate how to use generics with functions

--- features/generics.ts
+++ features/generics.ts
@@ -14,3 +14,15 @@ class ArrayOfAnything<T> {
 const arrayOfStrings = new ArrayOfAnything<string>(strings);
 /* Type inference */
 const arrayOfNumbers = new ArrayOfAnything(numbers);
+
+/* Functions */
+function printAnything<T>(collection: T[]): void {
+  for (let i = 0; i < collection.length; i++) {
+    console.log(collection[i]);
+  }
+}
+
+/* Type annotation */
+printAnything<string>(strings);
+/* Type inference */
+printAnything<number>(numbers);
