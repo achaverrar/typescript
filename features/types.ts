35f3f1df8b89181d9a7ddced7525c1db537ef393
Ayxa Chaverra <achaverrar@gmail.com>
2023-06-18 11:11:10 -0500

    expand on the examples about types to illustrate intersection types

--- features/types.ts
+++ features/types.ts
@@ -9,16 +9,6 @@ class Color {}
 
 const red = new Color();
 
-// Union types
-type UnionType = string | undefined;
-let response: UnionType;
-response?.toUpperCase();
-
-// Type Assertion
-let message: any = "";
-const uppercasedMessage = (message as string).toUpperCase();
-const canvas = <HTMLCanvasElement>document.getElementById("canvas");
-
 // Custom Types
 type Gender = "feminine" | "masculine" | "nonbinary" | "nonspecified";
 
@@ -29,9 +19,34 @@ type Person = {
   nationality: string;
 };
 
+type Student = {
+  school: string;
+};
+
 const robert: Person = {
   name: "Robert",
   age: 15,
   gender: "masculine",
   nationality: "Canadian",
 };
+
+// Intersection types
+type HighschoolStudent = Person & Student;
+
+const candance: HighschoolStudent = {
+  name: "Candance",
+  age: 13,
+  gender: "feminine",
+  nationality: "American",
+  school: "St. Patrick High School",
+};
+
+// Union types
+type UnionType = string | undefined;
+let response: UnionType;
+response?.toUpperCase();
+
+// Type Assertion
+let message: any = "";
+const uppercasedMessage = (message as string).toUpperCase();
+const canvas = <HTMLCanvasElement>document.getElementById("canvas");
